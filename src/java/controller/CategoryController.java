75dbb5e17f29d734b751f9bb80b63bf4d3b82407
Jose Fernandes <jofer.br@gmail.com>
2019-05-31 19:55:02 -0300

    modulos já pronto atualizar, inserir, deletar.

---
+++ src/java/controller/CategoryController.java
@@ -0,0 +1,108 @@
+
+package controller;
+
+import dao.CategoryHelper;
+import model.Film;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Named;
+import model.Category;
+
+/**
+ *
+ * @author NCCallegari
+ */
+@Named(value = "categoryController")
+@SessionScoped
+public class CategoryController implements Serializable {
+
+    int startId;
+    int endId;
+    DataModel categoryModel;
+    CategoryHelper helper;
+    private final int recordCount = 1000;
+    private final int pageSize = 10;
+    private Category category;
+    private Integer codigo;
+
+
+    public Category getCategory()
+    {
+        //return this.category;
+        if (category == null){
+            category = new Category();
+        }
+          return category;
+    }
+    
+    /**
+     * Creates a new instance of FilmController
+     */
+    public CategoryController() {
+        helper = new CategoryHelper();
+
+    }
+
+    public DataModel getCategoryModel() {
+        if (categoryModel == null) {
+            categoryModel = new ListDataModel(helper.getCategory());
+        }
+        return categoryModel;
+    }
+    
+   // public Category getCategory() {
+   //     if (category == null) {
+   //         category = new Category();
+   //     }
+  //      return category;
+  //  }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public void inserir() {
+        CategoryHelper categoryHelper = new CategoryHelper();
+        categoryHelper.inserir(category);
+    }
+
+    public void delete() {
+        CategoryHelper categoryHelper = new CategoryHelper();
+        categoryHelper.excluir(category);
+    }
+
+    public void atualizar() {
+        CategoryHelper categoryHelper = new CategoryHelper();
+        categoryHelper.atualizar(category);
+    }
+    
+    public void carregardados(){
+        
+        if(codigo != null){
+            
+            CategoryHelper categoryHelper = new CategoryHelper();
+            category = categoryHelper.getCategoryByID(codigo);
+        }else{
+            
+            category = new Category();
+        }
+        
+    }
+
+   // public DataModel getCategoryy() {
+   // if (categoryFilm == null) {
+   // categoryFilm = new ListDataModel(helper.getCategory());
+   // }
+   // return categoryFilm;
+  //  }    
+}
---
+++ src/java/dao/CategoryHelper.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import dvdrental.HibernateUtil;
+import model.Category;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author NCCallegari
+ */
+// Criação de uma classe chamada CategoryHelper
+public class CategoryHelper {
+
+    Session session = null; //Se a sessão não estiver aberta
+
+    public CategoryHelper() {
+        openSession();
+    }
+
+    private void openSession() {
+        if (session == null || !session.isOpen()) {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+        }
+    }
+
+    private void closeSession() {
+        if (session.isOpen()) {
+            session.close();
+        }
+    }
+
+    public List getCategory() {
+        openSession();
+        List<Category> categoryList = null;
+        org.hibernate.Transaction tx = session.beginTransaction();
+        try {
+            Query q = session.createQuery("from Category");
+            categoryList = (List<Category>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            closeSession();
+        }
+        return categoryList;
+    }
+
+    public void inserir(Category category) {
+        openSession();
+        org.hibernate.Transaction tx = session.beginTransaction();
+        try {
+            session.save(category);
+            tx.commit();
+
+        } catch (RuntimeException ex) {
+            if (tx != null) {
+                tx.rollback();
+
+            }
+           // throw ex;
+        } finally {
+            closeSession();
+
+        }
+    }
+
+    
+    public void excluir(Category category) {
+        openSession();
+        org.hibernate.Transaction tx = session.beginTransaction();
+        try {
+            session.delete(category);
+            tx.commit();
+
+        } catch (RuntimeException ex) {
+            if (tx != null) {
+                tx.rollback();
+
+            }
+            throw ex;
+        } finally {
+            closeSession();
+
+        }
+    }
+
+    
+    public void atualizar(Category category) {
+        openSession();
+        org.hibernate.Transaction tx = session.beginTransaction();
+        try {
+            session.update(category);
+            tx.commit();
+
+        } catch (RuntimeException ex) {
+            if (tx != null) {
+                tx.rollback();
+
+            }
+            throw ex;
+        } finally {
+            closeSession();
+
+        }
+    }
+
+        public Category getCategoryByID(int categoryId) {
+        openSession();
+        List<Category> categoryList = null;
+        org.hibernate.Transaction tx = session.beginTransaction();
+        try {
+            Query q = session.createQuery("from Category as category where category.categoryId=" + categoryId);
+            categoryList = (List<Category>) q.list();
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            closeSession();
+        }
+
+        return categoryList.get(0);
+    }
+    
+}
+
+
+    
+
